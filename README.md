e95205d8d063c975fc813c529c4d6cb71348a011
Sebastiaan Brand <sebastiaanbrand@gmail.com>
2025-02-13 14:23:19 +0100

    small updates for mtbdd experiments

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 1. Clone this repository including its submodules.
 ```shell
-$ git clone --recurse-submodules https://github.com/sebastiaanbrand/q-sylvan-benchmarks.git
+$ git clone --recurse-submodules <this repo's url>
 ```
 
 2. Install basic build tools.
--- scripts/generate_sim_experiments.py
+++ scripts/generate_sim_experiments.py
@@ -10,7 +10,7 @@ from pathlib import Path
 
 EXPERIMENTS_DIR = "experiments/"
 
-QSY_QASM = "timeout {} ./tools/q-sylvan/build/qasm/run_qasm_on_qmdd {} --workers {} {} --json {} &> {}\n"
+QSY_QASM = "timeout {} ./tools/q-sylvan/build/qasm/run_qasm_on_{} {} --workers {} {} --json {} &> {}\n"
 MQT_QASM = "timeout {} ./tools/mqt-ddsim/build/apps/mqt-ddsim-simple --simulate_file {} --shots 1 --ps --pm {} 2> {} 1> {}\n"
 QUA_QASM = "cd tools/Quasimodo/python_pkg; "\
            "timeout {} python quasimodo_qasm.py ../../../{} 2> ../../../{} 1> ../../../{}; "\
@@ -33,12 +33,13 @@ parser.add_argument('--nqubits', type=int, nargs='+', help="Only include circuit
 parser.add_argument('--min_qubits', type=int, help="Only include circuits with at leats min_qubits.")
 parser.add_argument('--max_qubits', type=int, help="Only include circuits up to max_qubits.")
 parser.add_argument('--norm_strat', choices=['low','max','min','l2'], default='max', help="Norm strat to use for all q-sylvan runs.")
+parser.add_argument('--dd_types', nargs='+', choices=['mtbdd','evdd'], default=['mtbdd'], help="DD type(s) to run for q-sylvan.")
 parser.add_argument('--wgt_tab_size', type=int, default=23, help="log2 of max edge weight table size.")
 parser.add_argument('--node_tab_size', type=int, default=25, help="log2 of max node table size.")
 parser.add_argument('--test_norm_strats', action='store_true', default=False, help="Run with different norm strats.")
 parser.add_argument('--test_inv_caching', action='store_true', default=False ,help="Run with both INV-CACHING on/off.")
 parser.add_argument('--test_reorder', action='store_true', default=False, help="Run with reorder qubits on/off.")
-parser.add_argument('--tools', nargs='+', default=['q-sylvan','mqt'], help="Which tools to include (q-sylvan, mqt, quasimodo).")
+parser.add_argument('--tools', nargs='+', default=['q-sylvan'], help="Which tools to include (q-sylvan, mqt, quasimodo).")
 parser.add_argument('--workers', nargs='+', default=[1], type=int, help="Run multicore benchmarks for given number of workers.")
 parser.add_argument('--timeout', action='store', default='10m', help='Timeout time per benchmark')
 parser.add_argument('--recursive', action='store_true', default=False, help="Recursively look for .qasm files in given dir.")
@@ -107,11 +108,14 @@ def experiments_sim_qasm(args):
 
     mqt_args = ''
     qsy_args = ''
+    qsy_mtbdd_args = ''
     if args.log_vector:
         mqt_args += '--pv'
         qsy_args += '--state-vector'
+        qsy_mtbdd_args += '--state-vector'
     if len(args.workers) == 1 and int(args.workers[0]) == 1:
         qsy_args += ' --count-nodes'
+        qsy_mtbdd_args += ' --count-nodes'
     inv_caching = ['', ' --disable-inv-caching'] if args.test_inv_caching else ['']
     reorder = ['', ' --reorder', ' --reorder-swaps'] if args.test_reorder else [' --reorder']
     norm_strats = [' -s low', ' -s max', ' -s min',  ' -s l2'] if args.test_norm_strats else [f' -s {args.norm_strat}']
@@ -183,7 +187,7 @@ def experiments_sim_qasm(args):
                                 'workers' : 1}, meta_file, indent=2)
 
             # Q-Sylvan (QMDD)
-            if 'q-sylvan' in args.tools:
+            if 'q-sylvan' in args.tools and 'evdd' in args.dd_types:
                 for w in args.workers:
                     for s in norm_strats:
                         for inv in inv_caching:
@@ -192,16 +196,34 @@ def experiments_sim_qasm(args):
                                 json_output = f"{output_dir}/json/{filename[:-5]}_qsylvan_{exp_counter}.json"
                                 log         = f"{output_dir}/logs/{filename[:-5]}_qsylvan_{exp_counter}.log"
                                 meta        = f"{output_dir}/meta/{filename[:-5]}_qsylvan_{exp_counter}.json"
-                                f_all.write(QSY_QASM.format(args.timeout, filepath, w, qsy_args+s+inv+r, json_output, log))
-                                f_qsy.write(QSY_QASM.format(args.timeout, filepath, w, qsy_args+s+inv+r, json_output, log))
+                                f_all.write(QSY_QASM.format(args.timeout, 'qmdd', filepath, w, qsy_args+s+inv+r, json_output, log))
+                                f_qsy.write(QSY_QASM.format(args.timeout, 'qmdd', filepath, w, qsy_args+s+inv+r, json_output, log))
                                 with open(meta, 'w', encoding='utf-8') as meta_file:
                                     json.dump({ 'circuit_type' : filename.split('_')[0],
                                                 'circuit' : filename[:-5],
+                                                'dd_type' : 'evdd',
                                                 'exp_id' : exp_counter,
                                                 'n_qubits' : get_num_qubits(filename),
                                                 'tool' : 'q-sylvan',
                                                 'norm_strat' : s.split()[-1],
                                                 'workers' : w}, meta_file, indent=2)
+            # Q-Sylvan (MTBDD)
+            if 'q-sylvan' in args.tools and 'mtbdd' in args.dd_types:
+                for w in args.workers:
+                    exp_counter += 1
+                    json_output = f"{output_dir}/json/{filename[:-5]}_qsylvan_{exp_counter}.json"
+                    log         = f"{output_dir}/logs/{filename[:-5]}_qsylvan_{exp_counter}.log"
+                    meta        = f"{output_dir}/meta/{filename[:-5]}_qsylvan_{exp_counter}.json"
+                    f_all.write(QSY_QASM.format(args.timeout, 'mtbdd', filepath, w, qsy_mtbdd_args, json_output, log))
+                    f_qsy.write(QSY_QASM.format(args.timeout, 'mtbdd', filepath, w, qsy_mtbdd_args, json_output, log))
+                    with open(meta, 'w', encoding='utf-8') as meta_file:
+                        json.dump({ 'circuit_type' : filename.split('_')[0],
+                                    'circuit' : filename[:-5],
+                                    'dd_type' : 'mtbdd',
+                                    'exp_id' : exp_counter,
+                                    'n_qubits' : get_num_qubits(filename),
+                                    'tool' : 'q-sylvan',
+                                    'workers' : w}, meta_file, indent=2)
 
 
 def main():
