8749d0161a5d1db189e8a387ebef5d230d896bc9
Sebastiaan Brand <sebastiaanbrand@gmail.com>
2025-03-05 11:30:28 +0100

    compute suggestions for delta

--- scripts/plot_error_bounds.py
+++ scripts/plot_error_bounds.py
@@ -1,6 +1,7 @@
 from decimal import Decimal, getcontext
 getcontext().prec = 128
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from process_results_plot import latex_float
 
@@ -8,20 +9,30 @@ from process_results_plot import latex_float
 LSTYLES = ['dashed','dotted','dashdot','solid']
 
 
-def error1(n, epsilon, vmax=1):
-    return Decimal(vmax)*(Decimal(1) + Decimal(epsilon))**Decimal(n) - Decimal(1)
+def error1(n, epsilon, approx=False):
+    if approx:
+        return Decimal(n) * Decimal(epsilon)
+    else:
+        return (Decimal(1) + Decimal(epsilon))**Decimal(n) - Decimal(1)
 
 
-def error2(n, delta, epsilon):
-    error2 = Decimal(0)
-    for j in range(n+1):
-        error2 += Decimal(2**j) * (Decimal(1) + Decimal(epsilon)) ** Decimal(j)
-    error2 *= Decimal(delta)
-    return error2
+def error2(n, delta, epsilon, approx=False):
+    if approx:
+        return Decimal(delta) * Decimal(2**(n+1))
+    else:
+        error2 = Decimal(0)
+        for j in range(n+1):
+            error2 += Decimal(2**j) * (Decimal(1) + Decimal(epsilon)) ** Decimal(j)
+        error2 *= Decimal(delta)
+        return error2
 
 
-def compute_error(n, delta, epsilon, vmax=1):
-    return error1(n, epsilon, vmax) + error2(n, delta, epsilon)
+def compute_error(n, delta, epsilon, approx=False):
+    return error1(n, epsilon, approx) + error2(n, delta, epsilon, approx)
+
+
+def suggest_delta(n, epsilon, error):
+    return (Decimal(error) - Decimal(n) * Decimal(epsilon)) / Decimal(2**(n+1))
 
 
 def plot_lines(lines_xs, lines_ys, labels, output_name, cosiness=1.7):
@@ -69,26 +80,39 @@ def plot_errors_deltas(ns, deltas, epsilon):
 
 def write_errors_1_and_2(ns, delta, epsilon):
     """
-    Write info for Table 1.
+    Write errors for concrete parameters.
     """
+    rows = []
     for n in ns:
-        print("n       =", n)
-        print(f"error_1 = {error1(n, epsilon):.3e}")
-        print(f"error_2 = {error2(n, delta, epsilon):.3e}")
-        print()
+        rows.append(
+            {'n' : n,
+             'epsilon' : f'{epsilon:.3e}',
+             'delta' : f'{delta:.3e}',
+             'error_1' : f'{error1(n, epsilon):.3e}',
+             'error_2' : f'{error2(n, delta, epsilon):.3e}'})
+    df = pd.DataFrame(rows)
+    print(df)
 
 
-def write_delta_suggestions(ns, error_targ):
+def write_delta_suggestions(ns, epsilon, errors):
     """
-    
+    For each n, give largest value of delta that still bounds error to `error`.
     """
-    print(ns)
-    print(error_targ)
+    rows = []
+    for error in errors:
+        for n in ns:
+            rows.append(
+                {'targ_error': f'{error:.3e}',
+                 'n' : n,
+                 'epsilon' : f'{epsilon:.3e}',
+                 'delta' : f'{suggest_delta(n, epsilon, error):.3e}'})
+    df = pd.DataFrame(rows)
+    print(df)
 
 
 def main():
     write_errors_1_and_2([10, 20, 30, 40, 50, 60], 1e-15, 1e-16)
-    write_delta_suggestions([10, 20, 30, 40, 50, 60], 1e-6)
+    write_delta_suggestions([10, 20, 30, 40, 50, 60], 1e-16, [1e-3, 1e-6])
     plot_errors_deltas(ns=range(10,21), deltas=[1e-10, 1e-8, 1e-6], epsilon=1e-16)
 
 
